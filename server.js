1f40263e8369d035730f4ebbd8c8bf57a5887434
Sriram <sri761177@gmail.com>
2024-03-28 10:16:32 +0530

    complete task 08

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const errorHandlingMiddleware = require('./middleware/errorHandlingMiddleware');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(errorHandlingMiddleware);
 
 // Routes
 app.use('/api', authRoutes);
